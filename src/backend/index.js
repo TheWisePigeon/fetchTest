561eddf6fb934d73f7449567935547b90146a6bc
TheWisePigeon <zozozozeph@gmail.com>
2022-03-17 22:29:52 +0000

    Update index.js

--- src/backend/index.js
+++ src/backend/index.js
@@ -2,25 +2,25 @@ var express = require('express')
 
 const app = express()
 
-app.get('/', (req, res)=>{
-    res.json(
-        
-            {
-                src : "",
-                name : ""
-            },
-            {
-                src : "",
-                name : ""
-            },
-            {
-                src : "",
-                name : ""
-            }
-        
-    )
+app.get('/', (req, res) => {
+    res.send(
+
+        {{
+        src: "",
+        name: ""
+    },
+        {
+            src: "",
+            name: ""
+        },
+        {
+            src: "",
+            name: ""
+        }}
+
+)
 })
 
-app.listen(5000, ()=>{
+app.listen(5000, () => {
     console.log("App listening on port 5000");
 })
